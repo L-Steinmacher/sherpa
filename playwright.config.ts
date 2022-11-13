8f883f0fdffce769217d164258b3a5863cbded83
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2022-11-12 19:28:44 -0800

    feat: finish playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,7 +58,7 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    port: 8811,
+    port: 3000,
   },
 };
 
