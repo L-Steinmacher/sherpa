893925eaf22fab943350913740e856c4bfdba15e
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-01-21 18:42:40 -0800

    feat: upgraded TS and updated join.tsx

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -1,51 +1,82 @@
-import type { ActionArgs, LoaderArgs, MetaFunction } from "@remix-run/node";
+import type { DataFunctionArgs, MetaFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 import * as React from "react";
 
 import { getUserId, createUserSession } from "~/session.server";
 
-import { createUser, getUserByEmail } from "~/models/user.server";
+import { createUser, getUserByEmail, getUserByUsername } from "~/models/user.server";
 import { safeRedirect, validateEmail } from "~/utils";
 
-export async function loader({ request }: LoaderArgs) {
+export async function loader({ request }: DataFunctionArgs) {
   const userId = await getUserId(request);
   if (userId) return redirect("/");
   return json({});
 }
 
-export async function action({ request }: ActionArgs) {
+export async function action({ request }: DataFunctionArgs) {
   const formData = await request.formData();
+  const username = formData.get("username");
   const email = formData.get("email");
   const password = formData.get("password");
   const redirectTo = safeRedirect(formData.get("redirectTo"), "/");
 
   if (!validateEmail(email)) {
     return json(
-      { errors: { email: "Email is invalid", password: null } },
+      { errors: { username: null,email: "Email is invalid", password: null } },
       { status: 400 }
     );
   }
 
   if (typeof password !== "string" || password.length === 0) {
     return json(
-      { errors: { email: null, password: "Password is required" } },
+      { errors: { username: null, email: null, password: "Password is required" } },
       { status: 400 }
     );
   }
 
   if (password.length < 8) {
     return json(
-      { errors: { email: null, password: "Password is too short" } },
+      { errors: { username: null,email: null, password: "Password is too short" } },
       { status: 400 }
     );
   }
+
+  if (typeof username !== "string" || username.length === 0 || username === null) {
+    return json(
+      { errors: { username: "Username is required", email: null, password: null } },
+      { status: 400 }
+    );
+  }
+
+  if (username.length < 6) {
+    return json(
+      { errors: { username: "Username is too short", email: null, password: null } },
+      { status: 400 }
+    );
+  }
+
+  const existingUsername = await getUserByUsername(username);
+  if (existingUsername) {
+    return json(
+      {
+        errors: {
+          username: "A user already exists with this username",
+          email: null,
+          password: null,
+        },
+      },
+      { status: 400 }
+    );
+  }
+
 
   const existingUser = await getUserByEmail(email);
   if (existingUser) {
     return json(
       {
         errors: {
+          username: null,
           email: "A user already exists with this email",
           password: null,
         },
@@ -54,7 +85,7 @@ export async function action({ request }: ActionArgs) {
     );
   }
 
-  const user = await createUser(email, password);
+  const user = await createUser(username, email, password);
 
   return createUserSession({
     request,
@@ -74,6 +105,7 @@ export default function Join() {
   const [searchParams] = useSearchParams();
   const redirectTo = searchParams.get("redirectTo") ?? undefined;
   const actionData = useActionData<typeof action>();
+  const usernameRef = React.useRef<HTMLInputElement>(null);
   const emailRef = React.useRef<HTMLInputElement>(null);
   const passwordRef = React.useRef<HTMLInputElement>(null);
 
@@ -82,7 +114,10 @@ export default function Join() {
       emailRef.current?.focus();
     } else if (actionData?.errors?.password) {
       passwordRef.current?.focus();
+    } else if (actionData?.errors?.username) {
+      usernameRef.current?.focus();
     }
+
   }, [actionData]);
 
   return (
@@ -90,6 +125,31 @@ export default function Join() {
       <div className="mx-auto w-full max-w-md px-8">
         <Form method="post" className="space-y-6">
           <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Username
+            </label>
+            <div className="mt-1">
+              <input
+                ref={usernameRef}
+                id="username"
+                required
+                name="username"
+                type="text"
+                autoComplete="username"
+                aria-invalid={actionData?.errors?.username ? true : undefined}
+                aria-describedby="username-error"
+                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              />
+              {actionData?.errors?.username && (
+                <div className="pt-1 text-red-700" id="username-error">
+                  {actionData.errors.username}
+                </div>
+              )}
+            </div>
+
             <label
               htmlFor="email"
               className="block text-sm font-medium text-gray-700"
@@ -166,6 +226,17 @@ export default function Join() {
           </div>
         </Form>
       </div>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>{error.message}</p>
+      <p>The stack trace is:</p>
+      <pre>{error.stack}</pre>
     </div>
   );
 }
