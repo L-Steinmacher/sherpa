58b406396cbe06cb082ac902ae52478dddaa26de
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-01-24 19:18:10 -0800

    feat: user page data displayed

--- app/routes/users_+/edit.tsx
+++ app/routes/users_+/edit.tsx
@@ -1,7 +1,24 @@
-import type { DataFunctionArgs } from "@remix-run/node";
+import type { DataFunctionArgs} from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { requireUserId } from "~/session.server";
+import { authenticator } from "~/utils/auth.server";
+import { prisma } from "~/utils/db.server";
 
 export async function loader({ request }: DataFunctionArgs) {
-  return {};
+  const userId = await requireUserId(request);
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+  if (!user) {
+    const requestUrl = new URL(request.url);
+    const loginParams = new URLSearchParams([
+      ["redirectTo", `${requestUrl.pathname}${requestUrl.search}`],
+    ]);
+    const redirectTo = `/login?${loginParams}`;
+    await authenticator.logout(request, { redirectTo });
+    return redirect(redirectTo);
+  }
+  return json({ user });
 }
 
 export async function action({ request }: DataFunctionArgs) {
